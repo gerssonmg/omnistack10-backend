2ba4e47be37c35949301f170c676a96d5c82ec30
Gerson <gersoncafilho@gmail.com>
2020-01-14 09:48:00 -0300

    Conexao com sucesso. Iniciando Routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //Metodos HTTP: GET, POST, PUT, DELETE
 
@@ -7,7 +8,15 @@ const express = require('express');
 // Route Params: request.params (Identificar um recurso na alteção ou remoção) -PUT-DELETE
 // Body:   -POST-PUT
 
+// MongoDB (Não-relacional)
+
 const app = express();
+
+mongoose.connect('mongodb+srv://gerssonmg:gerssonmg@cluster0-gvs8c.mongodb.net/week10?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
 app.use(express.json()); //de maneira simplificada, faz o express entender body em formato JSON
 
