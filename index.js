fa103af43040648bacf39fc8444c2d15792b97b0
Gerson <gersoncafilho@gmail.com>
2020-01-14 09:21:59 -0300

    Indo iniciar conexão com o bd

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 
+//Metodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parametros:
+// Query Params: request.query( Filtros, ordenação, paginação, ...) GET
+// Route Params: request.params (Identificar um recurso na alteção ou remoção) -PUT-DELETE
+// Body:   -POST-PUT
+
 const app = express();
+
+app.use(express.json()); //de maneira simplificada, faz o express entender body em formato JSON
 
 app.post('/', (request, response) => {
     return response.json({ message: 'Heldlo' });
